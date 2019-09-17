17509c17a68c5cf9070f1f84f354e5a62577b93d
Alex <alexilin83@gmail.com>
2019-09-17 18:57:33 +0300

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+const presets = [
+    [
+        "@babel/preset-env",
+        {
+            useBuiltIns: 'usage',
+            corejs: 3
+        }
+    ]
+];
+
+const plugins = [
+    "transform-custom-element-classes"
+];
+
+module.exports = { presets, plugins };
---
+++ src/js/App.svelte
@@ -0,0 +1,14 @@
+<style type="text/scss">
+    .greeting {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
+
+<script>
+    export let title;
+</script>
+
+<div class="greeting">
+    <h1>{title}</h1>
+</div>
